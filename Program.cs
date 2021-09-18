ff2768216c07b5ecdb41a7ade37a1395eb057e2e
AnaPaulaVeloso <velosoanapaula29@gmail.com>
2021-09-18 01:57:52 -0300

    Concluir a tarefa AreaTrianguloRet

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double medidabase,medidaAltura,medidaArea;
+            Console.Write("valor base.......:");
+            medidabase = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("valor altura.....:");
+            medidaAltura = Convert.ToDouble(Console.ReadLine());
+            medidaArea = (medidabase * medidaAltura)/2; 
+
+            Console.WriteLine($"Área do Triângulo:{medidaArea}");
+
+
+            
+        }
+    }
+}
